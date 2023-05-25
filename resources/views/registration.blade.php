a752807094297220cc6f38ca5f8c992098bb8cf4
johnstone gikenye <joegikenye@gmail.com>
2023-05-25 07:13:37 -0700

    Laravel 101 🤳

---
+++ resources/views/registration.blade.php
@@ -0,0 +1,23 @@
+@extends ('layout')
+@section ('title', 'registration')
+@section('content')
+<div class="container"
+
+<form class ="ms-auto me-auto" style ="width: 500px">
+  <div class="mb-3">
+    <label class="form-label">Full Name</label>
+    <input type="name" class="form-control" name="name">
+  </div>
+  <div class="mb-3">
+    <label class="form-label">Email address</label>
+    <input type="email" class="form-control" name="email" >
+  </div>
+  <div class="mb-3">
+    <label  class="form-label">Password</label>
+    <input type="password" class="form-control" name="password">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+</div>
+@endsection
